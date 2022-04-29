0dc475691e18cebdcf20de6b1c6e8ad6810647a7
Bernard Duggan <bernard@circl.es>
2022-04-29 17:19:22 +1000

    fix

--- test/absinthe/async_test.exs
+++ test/absinthe/async_test.exs
@@ -10,55 +10,6 @@ defmodule Absinthe.AsyncTest do
     :ok
   end
 
-  test "defer sync" do
-    socket = get_socket(0)
-    query = %{"query" => "query { slow_fields (delays: [400]) { value @defer } }"}
-    ref = push(socket, "doc", query)
-    refute_reply(ref, _, _, 200)
-
-    query = %{"query" => "query { slow_fields (delays: [100]) { value @defer } }"}
-    ref2 = push(socket, "doc", query)
-
-    # This second query will be blocked by the first because there are no async
-    # threads left to handle it
-    assert_reply(ref, :ok, %{queryId: id1}, 300)
-    assert_push("doc", reply)
-    assert reply.queryId == id1
-    assert reply.data == 400
-
-    assert_reply(ref2, :ok, %{queryId: id2}, 200)
-    assert_push("doc", reply)
-    assert reply.queryId == id2
-    assert reply.data == 100
-  end
-
-  test "repeated async defers" do
-    socket = get_socket(2)
-
-    # Run the test multiple times to ensure that completed async tasks are
-    # cleaned up and don't prevent subsequent queries being handled
-    # asynchronously
-    Enum.each(1..3, fn _ ->
-      query = %{"query" => "query { slow_fields (delays: [400]) { value @defer } }"}
-      ref = push(socket, "doc", query)
-      assert_reply(ref, :ok, %{queryId: id1})
-
-      query = %{"query" => "query { slow_fields (delays: [100]) { value @defer } }"}
-      ref = push(socket, "doc", query)
-      assert_reply(ref, :ok, %{queryId: id2})
-
-      # This second query is faster so its deferred result should arrive before
-      # the first query's
-      assert_push("doc", reply, 200)
-      assert reply.queryId == id2
-      assert reply.data == 100
-
-      assert_push("doc", reply, 400)
-      assert reply.queryId == id1
-      assert reply.data == 400
-    end)
-  end
-
   defp get_socket(max_async_procs) do
     {:ok, _, socket} =
       "asdf"
